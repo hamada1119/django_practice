013d3f5c160f3465d424b47fc47d45651a9886af
hamada1119 <76606177+hamada1119@users.noreply.github.com>
2024-03-12 11:02:20 +0900

    remove:sns/views.py メモ削除

--- sns/views.py
+++ sns/views.py
@@ -3,10 +3,6 @@ from django.shortcuts import render
 from .models import Message
 from .serializer import MessageSerializer
 
-# Create your views here.
-
-
-
 class MessageViewset(viewsets.ModelViewSet):
     serializer_class=MessageSerializer
     queryset=Message.objects.all()
